e122ef40a253f09225b355ddc42a4cac0c97f590
Henri <henkka.huhtala@hotmail.com>
2020-12-29 19:14:43 +0200

    Started working on tasks for levels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,29 +7,50 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public float timeBetweenTasks;
     public List<LevelTask> levelTasks;
+    public Transform cameraClosePosition, cameraFarPosition;
 
+    LevelTask currentTask;
     int currentTaskIndex;
     float timeToNextTask;
 
     void Start()
     {
         instance = this;
-        levelTasks[currentTaskIndex].StartTask();
-        timeToNextTask = levelTasks[currentTaskIndex].taskTime + timeBetweenTasks;
+        timeToNextTask = timeBetweenTasks;
     }
 
     void Update()
     {
         timeToNextTask -= Time.deltaTime;
-        if(timeToNextTask< 0)
+        if(timeToNextTask < 0)
         {
             NextTask();
+        }
+
+        PlayTask();
+    }
+
+    void PlayTask()
+    {
+        if (currentTask == null) return;
+
+        switch (currentTask.type)
+        {
+            case LevelTask.Type.Radar:
+                Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, cameraFarPosition.position, Time.deltaTime * 2.5f);
+                Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, cameraFarPosition.rotation, Time.deltaTime * 2.5f);
+                break;
+            case LevelTask.Type.Dialogue:
+                Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, cameraClosePosition.position, Time.deltaTime * 2.5f);
+                Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, cameraClosePosition.rotation, Time.deltaTime * 2.5f);
+                break;
+            default:
+                break;
         }
     }
 
     public void NextTask()
     {
-        currentTaskIndex++;
         if(currentTaskIndex >= levelTasks.Count)
         {
             //Level finished
@@ -37,5 +58,7 @@ public class GameManager : MonoBehaviour
         }
         levelTasks[currentTaskIndex].StartTask();
         timeToNextTask = levelTasks[currentTaskIndex].taskTime + timeBetweenTasks;
+        currentTask = levelTasks[currentTaskIndex];
+        currentTaskIndex++;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform player;
     public float moveSpeed;
     public int horizontalLimit;
 
+    float targetRotation;
 
     void Start()
     {
@@ -15,18 +17,26 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        player.Translate(Vector3.forward * Time.deltaTime * 14);
         GetInput();
     }
 
     void GetInput()
     {
-        Quaternion rot = Quaternion.identity;
-        rot.z = Input.GetAxis("Horizontal") * -0.15f;
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, Time.deltaTime * 50);
-
+        //Position
         Vector3 pos = transform.position;
         pos.x += Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed;
         pos.x = Mathf.Abs(pos.x) > horizontalLimit ? Mathf.Sign(pos.x) * horizontalLimit : pos.x;
         transform.position = pos;
+
+
+        //Rotation
+        Quaternion rot = Quaternion.identity;
+        float prevRotation = targetRotation;
+        targetRotation = Mathf.MoveTowards(prevRotation, (Input.GetAxisRaw("Horizontal") * -0.15f), Time.deltaTime / 2);
+        targetRotation = Mathf.MoveTowards(prevRotation, (Mathf.Abs(pos.x) >= horizontalLimit ? 0 : targetRotation), Time.deltaTime / 2);
+        rot.z = targetRotation;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, Time.deltaTime * 50);
+
     }
 }
